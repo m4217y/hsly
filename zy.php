af2dd533f4b73ef5d602a17662ec619b42d7e97c
寻客欢 <8489765+xkh1@user.noreply.gitee.com>
2021-09-23 17:21:08 +0000

    add zy.php.

---
+++ zy.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>网恋翻车之黑山老妖你别跑！！</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"7>
+<link rel="stylesheet" href="xkh/css/style.css">
+</head>
+
+<div id="hedadr">
+<img src="imgs/logo.gif">
+<ul>xkh</ul>
+<h3>念念不忘，必有回响！</h3></div>
+  <BODY>
+     <script>
+     function  create() {
+         var input = document.getElementById('content');
+             var kd = document.getElementById('kd');
+                 var myid = document.getElementById('myid');
+                     var url = document.getElementById('url');
+                         if (myid.value=="" || url.value==""){
+                                 alert("ID或跳转地址不能为空！");
+                                         return false;
+                                             }
+                                                 kd.href = 'https://xunhehuan.cn/xkh/?id='+myid.value+'&url='+url.value; 
+                                                     kd.style = ''; 
+                                                         kd.innerText = 'https://xunhehuan.cn/xkh/?id='+myid.value+'&url='+url.value; 
+                                                         }
+                                                            </script>
+                                                             
+                                                                
+                                                                   <div class="title" id="q">公告</div>
+                                                                   <div class="show" id="qx"> <h5>1.本工具仅做学习交流使用，请勿用于非法用途！后果自负！</h5>
+                                                                      <h5>2.懒得做数据库，ID是查看照片的凭证，不要泄露给知道这个平台的人</h5>
+                                                                         <h5>3.为节省服务器资源，不定期删除7天前的数据</h5></div>
+                                                                            
+                                                                               
+                                                                                     <div class="title" id="q">信息</div>
+                                                                                       <div<p>默认ID为 xkh 点击修改专属ID</p>
+                                                                                         <h3>输入ID：<input type= "text" id="myid"  value= 'xkh'/></h3>
+                                                                                            <h4>拍摄后跳转到：<input type= "text" id="url"  value= 'https://xunhehuan.cn'/></h4>
+                                                                                               </div>
+                                                                                                  
+                                                                                                     
+                                                                                                          <div class="title" id="q">生成</div>
+                                                                                                            <div class="show" id="qx"> <h2><input type= "button"  value= '生成链接骗妖'  onclick= 'create();'  />   <input type="button"       value='查看黑山老妖' onclick=window.location.href='zp.php?id='+document.getElementById('myid').value> </h2></div>
+                                                                                                               
+                                                                                                                  <div class="title" id="q">分享</div>
+                                                                                                                      <div class="show" id="qx"> <h5>将以下链接地址发送给你要拍摄的对象，对方进入后将会拍摄照片并保存。</h5>
+                                                                                                                         <h5><a id="kd" style="pointer-events: none;">请先生成链接！</a></h5>
+                                                                                                                            </div>
+                                                                                                                               
+                                                                                                                                    <div class="title" id="q">解答</div>
+                                                                                                                                     <div class="show" id="qx">  <h6>问题一：为什么拍摄的是黑屏？</h6> 
+                                                                                                                                      <h4> 答：因为该浏览器不支持，更换浏览器即可，安卓用户建议直接在QQ内打开链接。</h4>
+                                                                                                                                         <h6>问题二：拍摄的照片不全？</h6>
+                                                                                                                                            <h4>答：还没等跳转完成就关闭了页面，数据还没传输完成。</h4></div>
+                                                                                                                                              </BODY>
+                                                                                                                                              </HTML>
+                                                                                                                                              
